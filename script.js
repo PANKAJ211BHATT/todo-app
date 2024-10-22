4bf2265655f204d346f5d1fe554b061939940b83
PANKAJ BHATT <211pankajbhatt@gmail.com>
2024-10-22 15:52:02 +0530

    in this update we make logic for my add task and delete task

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ setInterval(() => {
 
 function addtask() {
     let inputfield = document.querySelector("#taskinputfield");
-    let inputvalue = inputfield.value;
+    let inputvalue = inputfield.value.trim().toUpperCase();
+
 
     if (inputvalue == "") {
         return
@@ -20,11 +21,41 @@ function addtask() {
     const taskdiv = document.createElement('div');
     taskdiv.classList.add('task-item');
 
-    taskdiv.textContent = inputvalue;
+    const checktask = document.createElement('input');
+    checktask.type = 'checkbox';
+    const tasktext = document.createElement('span');
+    tasktext.classList.add("tasktex");
+    tasktext.textContent = inputvalue;
+
+    const tasktime = document.createElement('p');
+    tasktime.classList.add('tasktime');
+
+    const time = new Date();
+    const hours = time.getHours();
+    const min = time.getMinutes();
+    tasktime.textContent = `${hours} : ${min}`;
+
+
+    const deletetask = document.createElement('button');
+    deletetask.textContent = 'Delete';
+    deletetask.onclick = function () {
+        taskdiv.remove();
+    }
+    const checkboxandtasktext = document.createElement("div");
+    checkboxandtasktext.classList.add("checkboxandtasktext");
+    const taskTimeAndDeleteButton = document.createElement("div");
+    taskTimeAndDeleteButton.classList.add("taskTimeAndDeleteButton");
+
+    checkboxandtasktext.appendChild(checktask);
+    checkboxandtasktext.appendChild(tasktext);
+    taskTimeAndDeleteButton.appendChild(tasktime);
+    taskTimeAndDeleteButton.appendChild(deletetask);
+
+    taskdiv.appendChild(checkboxandtasktext);
+    taskdiv.appendChild(taskTimeAndDeleteButton);
 
     let taskcontainer = document.querySelector(".taskCont");
-
-    taskcontainer.appendChild(taskdiv)
+    taskcontainer.appendChild(taskdiv);
 
     inputfield.value = "";
 }
