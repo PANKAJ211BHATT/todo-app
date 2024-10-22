9068e516f24737e33469a8c52a73ada87e10ed6e
PANKAJ BHATT <211pankajbhatt@gmail.com>
2024-10-22 21:04:16 +0530

    in this update we make a small calender on the right part of the page

--- script.js
+++ script.js
@@ -59,3 +59,65 @@ function addtask() {
 
     inputfield.value = "";
 }
+
+const calenderDates = document.getElementById("calender-dates");
+const Monthyear = document.querySelector(".Monthyear");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+
+
+let currentDate = new Date();
+
+function renderCalender() {
+    calenderDates.innerHTML = '';
+
+    let year = currentDate.getFullYear();
+
+
+
+    let month = currentDate.getMonth();
+    Monthyear.innerText = `${currentDate.toLocaleString('default', { month: 'long' })} ${year}`;
+
+    const firstDay = new Date(year, month, 1).getDay();
+    const totalday = new Date(year, month + 1, 0).getDate();
+
+
+    // making blank cells
+
+    for (let i = 0; i < firstDay; i++) {
+        const emptycell = document.createElement('div');
+        calenderDates.appendChild(emptycell);
+    }
+
+    /*creating date cells*/
+
+    for (let Day = 1; Day < totalday; Day++) {
+        const datecell = document.createElement('div')
+        datecell.innerText = Day;
+
+
+        if (Day === new Date().getDay() &&
+            month === new Date().getMonth() &&
+            year === new Date().getFullYear()) {
+            datecell.classList.add("current-date")
+        }
+        calenderDates.appendChild(datecell);
+
+    }
+
+
+
+
+}
+prev.addEventListener('click', () => {
+    currentDate.setMonth(currentDate.getMonth() - 1);
+    renderCalender();
+});
+
+next.addEventListener('click', () => {
+    currentDate.setMonth(currentDate.getMonth() + 1);
+    renderCalender();
+
+})
+
+renderCalender();
